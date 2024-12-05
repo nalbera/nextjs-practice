0c25592eb5829715c0c7c9bc2923b118606c2383
Nelson Albera <nalbera@hotmail.com>
2024-12-05 16:06:51 -0300

    add categories to landing

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
-import imgPortada from '../../public/img_portada.jpg';
+import imgPortada from "../../public/img_portada.jpg";
 import Link from "next/link";
+import smartPhone from "../../public/smartphone.jpg";
+import laptop from "../../public/laptop.jpg";
+import tablet from "../../public/tablet.jpg";
 
 export default function Landing() {
   return (
@@ -18,17 +21,112 @@ export default function Landing() {
                 consequuntur alias, maiores ut unde, cumque, ipsam deserunt ipsa
                 perferendis ratione. Sint, necessitatibus dolor.
               </p>
-              <Link href="/home" className="px-6 py-3 bg-white text-blue-600 font-bold rounded-full hover:bg-blue-700 transition duration-200">
+              <Link
+                href="/home"
+                className="px-6 py-3 bg-white text-blue-600 font-bold rounded-full hover:bg-blue-700 transition duration-200"
+              >
                 Entrar al Shop
               </Link>
             </div>
             <div className="md:w-1/2">
-                <Image
-                    className="w-full rounded-lg shadow-lg" 
-                    src={imgPortada}
-                    alt="imagen"
-                />
+              <Image
+                className="w-full rounded-lg shadow-lg"
+                src={imgPortada}
+                alt="imagen"
+              />
             </div>
+          </div>
+        </div>
+      </section>
+      <section className="p-20">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-gray-800 mb-8">
+            Categorías principales
+          </h2>
+
+          {/* smartphones */}
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div className="bg-white rounded-lg shadow-md overflow-hidden">
+              <Image
+                className="w-full h-64 object-cover"
+                src={smartPhone}
+                alt="smart-phone"
+              />
+              <div className="p-6">
+                <h3 className="text-xl font-bold text-gray-800 mb-2">
+                  Smatrphones
+                </h3>
+                <p className="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quisquam, aut consequuntur porro sed amet mollitia cum numquam
+                  soluta officiis.
+                </p>
+                <div className="mt-4 flex items-center justify-between">
+                  <span className="text-gray-700 font-medium">
+                    Desde $999.99
+                  </span>
+                  <button className="px-4 py-2 bg-blue-600 text-white font-bold rounded-full hover:bg-blue-700 transition duration-200">
+                    Ver mas...
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* -------------------------------------------------------------------- */}
+
+            {/* laptops */}
+            <div className="bg-white rounded-lg shadow-md overflow-hidden">
+              <Image
+                className="w-full h-64 object-cover"
+                src={laptop}
+                alt="laptop"
+              />
+              <div className="p-6">
+                <h3 className="text-xl font-bold text-gray-800 mb-2">
+                  Laptops
+                </h3>
+                <p className="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Provident nisi laborum omnis ipsam possimus tempora, suscipit,
+                  vel minima fugiat.
+                </p>
+                <div className="mt-4 flex items-center justify-between">
+                  <span className="text-gray-700 font-medium">
+                    Desde $888.88
+                  </span>
+                  <button className="px-4 py-2 bg-blue-600 text-white font-bold rounded-full hover:bg-blue-700 transition duration-200">
+                    Ver mas...
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* -------------------------------------------------------------------- */}
+
+            {/* tablet */}
+            <div className="bg-white rounded-lg shadow-md overflow-hidden">
+              <Image
+                className="w-full h-64 object-cover"
+                src={tablet}
+                alt="tablet"
+              />
+              <div className="p-6">
+                <h3 className="text-xl font-bold text-gray-800 mb-2">
+                  Tablets
+                </h3>
+                <p className="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Optio beatae placeat earum nostrum ab, saepe accusantium.
+                </p>
+                <div className="mt-4 flex items-center justify-between">
+                  <span className="text-gray-700 font-medium">
+                    Desde $888.88
+                  </span>
+                  <button className="px-4 py-2 bg-blue-600 text-white font-bold rounded-full hover:bg-blue-700 transition duration-200">
+                    Ver mas...
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* ------------------------------------------ */}
           </div>
         </div>
       </section>

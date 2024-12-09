06f7936d1f656ea062196134bc211292f42c6d3c
Nelson Albera <nalbera@hotmail.com>
2024-12-09 18:28:50 -0300

    updated productdetail component

--- front/src/components/detail/ProductDetailComponent.tsx
+++ front/src/components/detail/ProductDetailComponent.tsx
@@ -22,24 +22,17 @@ export const ProductDetailComponent: React.FC<DetailComponentProps> = ({id, name
               </h1>
               <div className="flex mb-4"></div>
               <p className="leading-relaxed">{description}</p>
+              <div>
+                <p className="text-base font-medium text-gray-900 title-font">
+                  Disponibes: {`${stock} unidades.`}
+                </p>
+              </div>
               <div className="flex pt-8">
                 <span className="text-2xl font-medium text-gray-900 title-font">
                   {`$ ${price}`}
                 </span>
                 <button className="flex px-6 py-2 ml-auto text-white bg-red-500 border-0 rounded focus:outline-none hover:bg-red-600">
-                  Button
-                </button>
-                <button className="inline-flex items-center justify-center w-10 h-10 p-0 ml-4 text-gray-500 bg-gray-200 border-0 rounded-full">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                  </svg>
+                  Comprar
                 </button>
               </div>
             </div>

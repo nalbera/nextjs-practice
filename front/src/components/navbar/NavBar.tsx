ae58aea91734a3d1f3e07f61851529169e486252
Nelson Albera <nalbera@hotmail.com>
2024-12-13 17:03:46 -0300

    modify navbar with context

--- front/src/components/navbar/NavBar.tsx
+++ front/src/components/navbar/NavBar.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { useContext } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../../public/logo2.jpg";
 import hamburger from "../../../public/hamburger.png";
 import close from "../../../public/close.png";
+import { AuthContext } from "@/context/AuthContextProvider";
 
 const NavBar: React.FC = () => {
 
@@ -16,6 +18,8 @@ const NavBar: React.FC = () => {
         }
     }
     
+    const { token, logout } = useContext(AuthContext);
+
   return (
     <nav className="flex flex-wrap items-center justify-between p-3 bg-teal-200/20">
       <Image
@@ -54,24 +58,46 @@ const NavBar: React.FC = () => {
         >
           Productos
         </Link>
-        <Link
-          href="/cart"
-          className="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none"
-        >
-          Carrito
-        </Link>
-        <Link
-           href="/login"
-           className="toggle hidden md:flex w-full md:w-auto px-3 py-3 text-right  hover:text-teal-500 text-teal-900"
-        >
-          Login
-        </Link>
-        <Link
-           href="/register"
-           className="toggle hidden md:flex w-full md:w-auto px-3 py-3 text-right  hover:text-teal-500 text-teal-900"
-        >
-          Register
-        </Link>
+        {
+            token ? (
+              <>
+                <Link
+                  href="/cart"
+                  className="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none"
+                >
+                  Carrito
+                </Link>
+                <Link
+                  href="/dashboard"
+                  className="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none"
+                >
+                  Perfil
+                </Link>
+                <p
+                  className="block cursor-pointer md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none"
+                  onClick={() => logout()}
+                >
+                  Cerrar Sesión
+                </p>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/login"
+                  className="toggle hidden md:flex w-full md:w-auto px-3 py-3 text-right  hover:text-teal-500 text-teal-900"
+                >
+                  Login
+                </Link>
+                <Link
+                  href="/register"
+                  className="toggle hidden md:flex w-full md:w-auto px-3 py-3 text-right  hover:text-teal-500 text-teal-900"
+                >
+                  Register
+                </Link>
+              </>
+            )
+        }
+        
       </div>
       
     </nav>

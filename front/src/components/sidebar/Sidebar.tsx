b8b3ea7416e894801fb2dcfb6b14f001aafb40cd
Nelson Albera <nalbera@hotmail.com>
2024-12-18 17:24:16 -0300

    modify sidebar

--- front/src/components/sidebar/Sidebar.tsx
+++ front/src/components/sidebar/Sidebar.tsx
@@ -26,12 +26,6 @@ const Sidebar: React.FC = () => {
                     <span className="text-sm font-medium">Compras</span>
                 </Link>
             </li>
-            <li>
-                <Link href="#" className='flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800'>
-                    <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"> <Image src={favorite} alt='image'/> </span>
-                    <span className="text-sm font-medium">Favoritos</span>
-                </Link>
-            </li>
             <hr/>
             <li>
                 <Link href="/home" className='flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800'>

6fbcadb33512af3809be67c4e569a0764425173e
Nelson Albera <nalbera@hotmail.com>
2024-12-20 18:34:30 -0300

    add page orders

---
+++ front/src/app/dashboard/orders/Orders.tsx
@@ -0,0 +1,9 @@
+
+
+export const Orders = () => {
+  return (
+    <div>Orders</div>
+  )
+}
+
+export default Orders;
--- front/src/components/sidebar/Sidebar.tsx
+++ front/src/components/sidebar/Sidebar.tsx
@@ -21,7 +21,7 @@ const Sidebar: React.FC = () => {
                 </Link>
             </li>
             <li>
-                <Link href="#" className='flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800'>
+                <Link href="/dashboard/orders" className='flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800'>
                     <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"> <Image src={cart} alt='image'/> </span>
                     <span className="text-sm font-medium">Compras</span>
                 </Link>

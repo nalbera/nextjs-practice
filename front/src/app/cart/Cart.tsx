c3e07f59d1d4877a0be6eaf561e3f1a417d9c833
Nelson Albera <nalbera@hotmail.com>
2024-12-23 10:45:06 -0300

    first commit orders

--- front/src/app/cart/Cart.tsx
+++ front/src/app/cart/Cart.tsx
@@ -36,7 +36,7 @@ const Cart: React.FC = () => {
                 progress: true,
                 position: "top-center",
                 transition: "bounceIn",
-                icon: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>',
+                icon: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.75" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check"><path d="M20 6 9 17l-5-5"/></svg>',
                 sonido: false,
             });   
         } catch (error) {
--- front/src/app/dashboard/orders/Orders.tsx
+++ front/src/app/dashboard/orders/Orders.tsx
@@ -1,8 +1,70 @@
+"use client"
 
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "@/context/AuthContextProvider";
+import { Product } from "@/types/product";
+
+interface Order {
+  id: number;
+  status: string,
+  date: string;
+  products: Product[]
+}
 
 export const Orders = () => {
-  return (
-    <div>Orders</div>
+  
+  const { token } = useContext(AuthContext);
+
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+
+    const getOrders = async () => {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/orders`, {
+        headers: {
+          'authorization': token
+        } as HeadersInit
+      });
+
+      const json = await response.json();
+      setOrders(json);
+      console.log(json);
+      
+    }
+
+    getOrders();
+  }, []);
+
+  return orders ? (
+    <div className="mt-14 mr-auto ml-14">
+      <div className="mb-8">
+        <h2 className="text-xl font-semibold">Compras realizadas</h2>  
+      </div>
+      {
+        orders.map((order) => (
+          <div key={order.id}>
+            <div className="flex bg-slate-200">
+              <h3>Fecha de compra: {new Date(order.date).toLocaleString()}</h3>
+              <span className="ml-10">Estado: {order.status == 'approved' ? "Aprobado" : "Rechazado"}</span>
+            </div>
+            {
+              order.products.map((product) => (
+                  <div key={product.id} className="flex bg-slate-100">
+                    <div>
+                      <img src={product.image} alt="product" width={100} height={100} />
+                    </div>
+                    <div>
+                      {product.name}
+                    </div>
+                  </div>
+              ))
+            }
+          </div>
+        ))
+      }
+    </div>
+  ) : (
+    <h3>No hay ordenes de compras para mostrar</h3>
   )
 }
 
